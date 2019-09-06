a3241162a81d61f059adac2c6a3379eead69c285
Kawaiisun <kawaiisungames@gmail.com>
2019-09-05 20:33:56 -0500

    app ui layout config

--- SimpleChat.py
+++ SimpleChat.py
@@ -28,9 +28,46 @@ class SimpleChatApp (Frame):
         label["fg"] = "white"
         label["bg"] = self["bg"]
         label["anchor"] = "w"
-        label["padx"] = 10
+        label.pack(fill="x", side="top", padx=10, pady=(10,0))
 
+        label = Label(self, text="an open source peer to peer chat app")
+        label["font"] = ("Helvetica", 12)
+        label["fg"] = "#cccccc"
+        label["bg"] = self["bg"]
+        label["anchor"] = "w"
+        label["padx"] = 10
+        label.pack(fill="x", side="top", ipadx=10)
+
+        frame = Frame(self, bg="#173f5f", height=450)
+        frame.pack(fill="x", side="top", anchor="n", padx=10, pady=(15,0))
+        frame.pack_propagate(0)
+
+        label = Label(frame, text="bot: welcome!")
+        label["font"] = ("Helvetica", 12)
+        label["fg"] = "#f6d55c"
+        label["bg"] = frame["bg"]
+        label["anchor"] = "w"
+        label["padx"] = 10
         label.pack(fill="x", side="top", ipadx=10, ipady=10)
+
+        frame = Frame(self, bg="#173f5f", height=45)
+        frame.pack(fill="x", side="top", anchor="n", padx=10, pady=(10,0))
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=0, minsize=43)
+        #frame.pack_propagate(0)
+
+        self.input = StringVar()
+        entry = Entry(frame, textvariable=self.input, relief=FLAT)
+        entry["font"] = ("Helvetica", 12)
+        entry["fg"] = "#3caea3"
+        entry["bg"] = frame["bg"]
+        entry.grid(column=0, row=0, sticky=W+E+N+S, padx=10, pady=10)
+        #entry.pack(fill="x", side="top", padx=10, pady=10)
+        self.input.set("enter a message here...")
+
+        button = Button(frame, relief=FLAT, bg="white")
+        button.grid(column=1, row=0, sticky=W+E+N+S, padx=5, pady=5)
+
 
     def startApp (self):
 
